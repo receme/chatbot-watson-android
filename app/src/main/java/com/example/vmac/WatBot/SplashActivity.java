f5423739dd933384e23e08a49b33968ef8e4ea9e
tahmid <tahmid.mozaffar@infolytx.com>
2017-04-26 12:16:13 +0600

    fixed splash screen, some ui changed

--- app/src/main/java/com/example/vmac/WatBot/SplashActivity.java
+++ app/src/main/java/com/example/vmac/WatBot/SplashActivity.java
@@ -1,16 +1,25 @@
 package com.example.vmac.WatBot;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
     }
 }
